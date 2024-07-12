a9dbc34e51bc34d522b64b99650a4fb5c9d70a26
14569 <165989521+kk14569@users.noreply.github.com>
2024-07-12 20:14:53 +0800

    Create hubs.is-an.app.js

---
+++ domains/hubs.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+    description: 'A chinese community.',
+    domain: 'is-an.app',
+    subdomain: 'hubs',
+    owner: {
+      email: '3754688512@qq.com',
+    },
+    record: {
+      CNAME: 'cname.freeflarum.com'
+    },
+    proxy: false
+  });
